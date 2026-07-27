fc02a90e51f928843fb771ae17857b4672c7b3f2
sanaamanat <sanaamanat844@gmail.com>
2026-07-26 22:56:57 -0700

    Exploratory Data Analysis

--- data_analysis.py
+++ data_analysis.py
@@ -1,7 +1,37 @@
 import pandas as pd
 
-# Read the dataset
-df = pd.read_csv("dataset/amazon_reviews.csv")
+reviews = []
 
-# Display the first five rows
+with open("dataset/train.ft.txt", "r", encoding="utf-8") as file:
+
+    for i, line in enumerate(file):
+
+        if i == 10000:
+            break
+
+        line = line.strip()
+
+        sentiment, review = line.split(" ", 1)
+
+        reviews.append([sentiment, review])
+
+df = pd.DataFrame(reviews, columns=["Sentiment", "Review"])
+
+print("Dataset Loaded Successfully!\n")
+
 print(df.head())
+
+print("\nShape of Dataset:")
+print(df.shape)
+
+print("\nColumn Names:")
+print(df.columns)
+
+print("\nDataset Information:")
+df.info()
+
+print("\nMissing Values:")
+print(df.isnull().sum())
+
+print("\nDuplicate Reviews:")
+print(df.duplicated().sum())
