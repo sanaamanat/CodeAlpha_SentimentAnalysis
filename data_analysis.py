e90002a738161ca001999d12b6da3748180a639e
sanaamanat <sanaamanat844@gmail.com>
2026-07-26 23:04:29 -0700

    distribution visualization

--- data_analysis.py
+++ data_analysis.py
@@ -1,9 +1,10 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 reviews = []
 
 with open("dataset/train.ft.txt", "r", encoding="utf-8") as file:
-
     for i, line in enumerate(file):
 
         if i == 10000:
@@ -15,10 +16,10 @@ with open("dataset/train.ft.txt", "r", encoding="utf-8") as file:
 
         reviews.append([sentiment, review])
 
+# Create DataFrame
 df = pd.DataFrame(reviews, columns=["Sentiment", "Review"])
 
 print("Dataset Loaded Successfully!\n")
-
 print(df.head())
 
 print("\nShape of Dataset:")
@@ -35,3 +36,27 @@ print(df.isnull().sum())
 
 print("\nDuplicate Reviews:")
 print(df.duplicated().sum())
+
+# Convert labels to readable text
+df["Sentiment"] = df["Sentiment"].replace({
+    "__label__1": "Negative",
+    "__label__2": "Positive"
+})
+
+print("\nUpdated Dataset:")
+print(df.head())
+
+print("\nSentiment Count:")
+print(df["Sentiment"].value_counts())
+
+# Create visualization
+plt.figure(figsize=(6, 4))
+
+sns.countplot(data=df, x="Sentiment")
+
+plt.title("Sentiment Distribution")
+plt.xlabel("Sentiment")
+plt.ylabel("Number of Reviews")
+
+plt.savefig("images/sentiment_distribution.png")
+plt.show()
